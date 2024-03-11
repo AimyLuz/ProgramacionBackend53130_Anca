3eccabf51b8c9a20b1feed8f07132dd0f3b54b7a
AimyLuz <ayelen.anca@gmail.com>
2024-03-11 11:56:38 -0300

    solucion test q me olvide de subir

--- desafio3/src/productmanager.js
+++ desafio3/src/productmanager.js
@@ -14,7 +14,7 @@ class ProductManager {
             if (!fileExists) {
                 // Si el archivo no existe, crear uno nuevo con una lista vacía de productos
                 await fs.promises.writeFile(this.path, '[]');
-                this.products = [];
+                return this.products = [];
             } else {
                 // Si el archivo existe, leer los productos desde el archivo
                 this.products = await this.getProduct();
@@ -114,55 +114,62 @@ class ProductManager {
 
 /*
 const productos = new ProductManager(path.join(__dirname, "./listadoDeProductos.json"));
-
+test = async () => {
 //TESTING
 //Primer llamada = arreglo vacio
-console.log("Primer llamado Array vacio")
-console.log(productos.getProduct());
-
-//Agrego productos
-console.log("Agregamos productos")
-productos.addProduct("Manzana", "es una fruta, puede ser roja o verde", 500, "Imaginate una foto de una manzana", "abc123", 20);
-productos.addProduct("Pera", "es una fruta, hace bien si estas mal de la panza", 400, "FotoDePera", "abc124", 30);
-productos.addProduct("Lechuga", "es una verdura verde", 100, "imagenLechu", "abc126", 50)
-productos.addProduct("Frutilla", "es una fruta rica con crema", 700, "FotoDeFrutilla", "abc125", 30);
-
-//Validacio de codigo repetido
-console.log("Intentamos agregar un producto con el codigo repetido")
-productos.addProduct("Gato", "es un animal", 0, "FotoDeGatito", "abc123", 1);
-
-//Segundo llamado de productos
-console.log("Listado de productos 2do llamado");
-console.log(productos.getProduct());
-
-//Validación de campos faltantes
-console.log("Se va a enviar un producto con campos faltantes")
-productos.addProduct("MotoG82", 50000, "ImagenMotoG", "aaabbb", 30);
-
-//buscar productos por ID
-console.log("Se va a buscar un producto que existe por el ID")
-productos.getProductById(2);
-
-//Producto no encontrado
-console.log("Se va a buscar un producto que NO existe por el ID")
-productos.getProductById(5);
-
-
-//Probamos eliminar un producto
-console.log("Probamos eliminar la frutilla");
-productos.deleteProduct(4)
-
-//Probamos que pasa si no encuentra el id del producto a eliminar
-console.log("probamos buscar un id que no existe para eliminar")
-productos.deleteProduct(15)
-
-//Probamos editar un producto
-console.log("probamos editar la lechuga")
-productos.updateProduct(3, "price", 200)
-
-//Probamos que pasa si no encuentra el id del producto a editar
-console.log("probamos buscar un id que no existe para editar");
-productos.deleteProduct(8, "price", 52);
+try{
+    //Primer llamada = arreglo vacio
+    console.log("Primer llamado Array vacio")
+    console.log( await productos.getProduct());
+    
+    //Agrego productos
+    console.log("Agregamos productos")
+    await productos.addProduct("Manzana", "es una fruta, puede ser roja o verde", 500, "Imaginate una foto de una manzana", "abc123", 20);
+    await productos.addProduct("Pera", "es una fruta, hace bien si estas mal de la panza", 400, "FotoDePera", "abc124", 30);
+    await productos.addProduct("Lechuga", "es una verdura verde", 100, "imagenLechu", "abc126", 50)
+    await productos.addProduct("Frutilla", "es una fruta rica con crema", 700, "FotoDeFrutilla", "abc125", 30);
+    
+    //Validacio de codigo repetido
+    console.log("Intentamos agregar un producto con el codigo repetido")
+    await productos.addProduct("Gato", "es un animal", 0, "FotoDeGatito", "abc123", 1);
+    
+    //Segundo llamado de productos
+    console.log("Listado de productos 2do llamado");
+    console.log( await productos.getProduct());
+    
+    //Validación de campos faltantes
+    console.log("Se va a enviar un producto con campos faltantes")
+    await productos.addProduct("MotoG82", 50000, "ImagenMotoG", "aaabbb", 30);
+    
+    //buscar productos por ID
+    console.log("Se va a buscar un producto que existe por el ID")
+    await productos.getProductById(2);
+    
+    //Producto no encontrado
+    console.log("Se va a buscar un producto que NO existe por el ID")
+    await productos.getProductById(5);
+    
+    
+    //Probamos eliminar un producto
+    console.log("Probamos eliminar la frutilla");
+    await productos.deleteProduct(4)
+    
+    //Probamos que pasa si no encuentra el id del producto a eliminar
+    console.log("probamos buscar un id que no existe para eliminar")
+    await productos.deleteProduct(15)
+    
+    //Probamos editar un producto
+    console.log("probamos editar la lechuga")
+    await productos.updateProduct(3, "price", 200)
+    
+    //Probamos que pasa si no encuentra el id del producto a editar
+    console.log("probamos buscar un id que no existe para editar");
+    await productos.deleteProduct(8, "price", 52);
+    }catch(error){
+        console.error("Error en la prueba:", error);
+    }
+    };
+    test();
 */
 module.exports = ProductManager;
 
